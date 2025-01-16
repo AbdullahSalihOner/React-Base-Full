b9da72ab56c15d5efee3091abb96fdd74e2d5483
Abdullah Salih Öner <64440843+AbdullahSalihOner@users.noreply.github.com>
2025-01-16 15:37:44 +0300

    context api project done

--- 7_context_api/src/context/task.js
+++ 7_context_api/src/context/task.js
@@ -8,7 +8,7 @@ function Provider({ children }) {
   const [tasks, setTasks] = useState([]);
   const createTask = async (title, taskDesc) => {
     debugger;
-    const response = await axios.post('http://localhost:3000/tasks', {
+    const response = await axios.post('http://localhost:3004/tasks', {
       title,
       taskDesc,
     });
@@ -17,19 +17,19 @@ function Provider({ children }) {
     setTasks(createdTasks);
   };
   const fetchTasks = async () => {
-    const response = await axios.get('http://localhost:3000/tasks');
-    debugger;
+    const response = await axios.get('http://localhost:3004/tasks');
+ 
     setTasks(response.data);
   };
   const deleteTaskById = async (id) => {
-    await axios.delete(`http://localhost:3000/tasks/${id}`);
+    await axios.delete(`http://localhost:3004/tasks/${id}`);
     const afterDeletingTasks = tasks.filter((task) => {
       return task.id !== id;
     });
     setTasks(afterDeletingTasks);
   };
   const editTaskById = async (id, updatedTitle, updatedTaskDesc) => {
-    await axios.put(`http://localhost:3000/tasks/${id}`, {
+    await axios.put(`http://localhost:3004/tasks/${id}`, {
       title: updatedTitle,
       taskDesc: updatedTaskDesc,
     });
